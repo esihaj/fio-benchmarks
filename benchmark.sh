280b4e06cbe577797c4c88d145e12c039b367ee7
Ehsan Hajyasini <esihaj@gmail.com>
2023-01-30 15:54:11 -0800

    separate iops and depth experiments

--- benchmark.sh
+++ benchmark.sh
@@ -12,14 +12,11 @@ ARCHIVE=$SUMMARY/archive
 
 mkdir $SUMMARY $RESULT $ARCHIVE || true
 
-for DEPTH in 1 2 4 8 16 32 64 128 192 256 512
-do
-    for IOPS in 100000 200000 300000 400000 500000 600000 800000 800000 810000 820000 830000 840000 850000 860000 870000 890000 900000 0
-    do
-        printf "depth = $DEPTH, IOPS=$IOPS, JOBS=$JOBS ...    "
-        export DEPTH
-        export IOPS
+run_benchmark() {
+        export DEPTH=$1
+        export IOPS=$2
         export JOBS
+        printf "depth=$DEPTH, IOPS=$IOPS, JOBS=$JOBS ...    "
         TEST_NAME="d-$DEPTH-iops-$IOPS"
         rm -rf $RESULT || true
         mkdir $RESULT
@@ -27,6 +24,21 @@ do
         fio $JOB > $SUMMARY/"$TEST_NAME.txt"
         mv $RESULT $ARCHIVE/$TEST_NAME
         echo "done"
-        sleep 1s
-    done
+        sleep 10s
+}
+
+run_benchmark 512 0
+exit 1
+
+for DEPTH in 1 2 4 8 16 32 64 128 192 256 512
+do
+    run_benchmark $DEPTH 0
+done
+
+mv $SUMMARY depth-summary
+mkdir -p $SUMMARY $ARCHIVE 
+
+for IOPS in 100000 200000 300000 400000 500000 600000 800000 800000 810000 820000 830000 840000 850000 860000 870000 890000 900000 0
+do
+    run_benchmark 128 $IOPS
 done
