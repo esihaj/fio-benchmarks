ab5b1bc899e9c6c3faccea0a21d55180d65921ff
Ehsan Hajyasini <esihaj@gmail.com>
2023-01-29 22:08:46 -0800

    add benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+JOB=two-disk-per-job.fio
+JOBS=2
+
+SUMMARY=summary
+RESULT=result
+ARCHIVE=$SUMMARY/archive
+
+mkdir $SUMMARY $RESULT $ARCHIVE
+
+for DEPTH in 1 2 4 8 16 32 64 128 192 256 512
+do
+    for IOPS in 100000 200000 300000 400000 500000 600000 800000 800000 810000 820000 830000 840000 850000 860000 870000 890000 900000 0
+    do
+        echo "depth = $DEPTH, IOPS=$IOPS, JOBS=$JOBS"
+
+        TEST_NAME="d$DEPTH-iops$IOPS"
+        rm -rf $RESULT || true
+        mkdir $RESULT
+        
+        fio $JOB > $SUMMARY/"$TEST_NAME.txt"
+        mv $RESULT $ARCHIVE/$TEST_NAME
+    done
+done
