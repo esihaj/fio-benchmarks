4edbb48b76ae98764ae236244399f1907ba7347b
Ehsan Hajyasini <esihaj@gmail.com>
2023-01-29 22:18:59 -0800

    fix benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -10,19 +10,23 @@ SUMMARY=summary
 RESULT=result
 ARCHIVE=$SUMMARY/archive
 
-mkdir $SUMMARY $RESULT $ARCHIVE
+mkdir $SUMMARY $RESULT $ARCHIVE || true
 
 for DEPTH in 1 2 4 8 16 32 64 128 192 256 512
 do
     for IOPS in 100000 200000 300000 400000 500000 600000 800000 800000 810000 820000 830000 840000 850000 860000 870000 890000 900000 0
     do
-        echo "depth = $DEPTH, IOPS=$IOPS, JOBS=$JOBS"
-
-        TEST_NAME="d$DEPTH-iops$IOPS"
+        printf "depth = $DEPTH, IOPS=$IOPS, JOBS=$JOBS ...    "
+        export DEPTH
+        export IOPS
+        export JOBS
+        TEST_NAME="d-$DEPTH-iops-$IOPS"
         rm -rf $RESULT || true
         mkdir $RESULT
         
         fio $JOB > $SUMMARY/"$TEST_NAME.txt"
         mv $RESULT $ARCHIVE/$TEST_NAME
+        echo "done"
+        sleep 1s
     done
 done
